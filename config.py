698c85604604f0c1f342cb1ae15ec848aa5137cc
Pugsley <piusmakene09@gmail.com>
2025-02-18 18:02:01 +0300

    configuration

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('FLASK_KEY')
+    SECRET_KEY = os.environ.get('FLASK_KEY', 'default-secret-key')
+
     # Database Configuration
     DB_USER = os.environ.get('DB_USER')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
@@ -18,3 +19,5 @@ class Config:
         SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     else:
         SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI", "sqlite:///chemicals.db")  # Fallback to SQLite
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
